986f1ff31b7d7f9662d65fd05c2024edd0d1ec2d
devchea <rchea817@gmail.com>
2020-05-07 09:11:43 -0700

    ship v1.added username

--- Game/main_game.rb
+++ Game/main_game.rb
@@ -1,3 +1,6 @@
+$name = " "
+
+
 def welcome #welcome to the game
   logo
   empty_line
@@ -10,9 +13,11 @@ def start_game
   prompt = TTY::Prompt.new
   input = prompt.select("Would you like to play the game?", %w(Yes No))
   if input == "Yes"
+    empty_line
+    get_username
     clear
     acceptance_letter
-    puts "You are now entering Hogwarts!"
+    puts "Welcome, #{$name}! You are now entering Hogwarts!"
     empty_line
     level_output
     continue
@@ -20,6 +25,11 @@ def start_game
     clear    #clears the screen
     abort "Goodbye" #exits the program
   end
+end
+
+def get_username
+  puts "What is your name?"
+  $name = gets.chomp
 end
 
 def prompt_questions
@@ -35,7 +45,7 @@ end
 def winner_prompt
   clear
   File.readlines("./Text_Images/winner.txt").each {|line| puts line}
-  puts "You answered #{$question_count} questions correctly!"
+  puts "#{$name}, you answered #{$question_count} questions correctly!"
   empty_line
   abort
 end
--- Game/question.rb
+++ Game/question.rb
@@ -1,6 +1,3 @@
-# require_relative './config/environment'
-# require 'tty-prompt'
-# prompt = TTY::Prompt.new
 require 'pry'
 
 $level = 1
